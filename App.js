44a9f68957979bf2bf4c77f5d6c311749b410fd5
jezrelljolampong <jezrelljolampong17@gmail.com>
2021-01-24 15:35:35 +0800

    Added content to ProductOverview screen and navigation

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import productReducers from './store/redcucers/productReducers'
+import ShopNavigator from './navigation/ShopNavigator'
+
+const rootReducer = combineReducers({
+  products: productReducers,
+})
+
+const store = createStore(rootReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Fuck test </Text>
-      <Button title='testtest' />
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,20 @@
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
+import Colors from '../constants/Colors'
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Colors.primary,
+      },
+      headerTintColor: 'white',
+    },
+  }
+)
+
+export default createAppContainer(ProductsNavigator)
